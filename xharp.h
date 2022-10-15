7c0959cfcfc151295aa77a6d9b2dff53db36fd85
qylWheels <command1748165360@126.com>
2022-10-15 17:20:13 +0800

    add xharp-lib basic definition

---
+++ xharp.h
@@ -0,0 +1,13 @@
+#ifndef XHARPLIB_XHARP_H_INCLUDED
+#define XHARPLIB_XHARP_H_INCLUDED
+
+#ifdef __cplusplus
+# define __XHARPLIB_BEGIN_DECLS extern "C" {
+# define __XHARPLIB_END_DECLS }
+#else  /* __cplusplus */
+# define __XHARPLIB_BEGIN_DECLS  /* empty */
+# define __XHARPLIB_END_DECLS  /* empty */
+#endif  /* !__cplusplus */
+
+#endif
+
